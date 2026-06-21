8826669f5a1c27f2fa0de97ad13fc325bef5c643
ACDPGestiondeAfiliados <consultas.acdp@gmail.com>
2026-06-20 23:58:48 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,29 @@
- ===============================
- CONFIG FIREBASE ACDP
- ===============================
+// ===============================
+// FIREBASE ACDP
+// ===============================
+
 
 const firebaseConfig = {
 
-    apiKey TU_API_KEY,
-    authDomain TU_PROYECTO.firebaseapp.com,
-    projectId TU_PROYECTO,
+apiKey: "AIzaSyBuXoGjEGxmGXuvrWSGbJW_-i8NDydJX38",
+
+authDomain:
+"acdp-afiliados.firebaseapp.com",
+
+projectId:
+"acdp-afiliados",
+
+storageBucket:
+"acdp-afiliados.firebasestorage.app",
+
+messagingSenderId:
+"67829346831",
+
+appId:
+"1:67829346831:web:2abdb4c55b504b752ce97a"
 
 };
+
 
 
 firebase.initializeApp(firebaseConfig);
